de3a64763d7204f739a81be869e258b2a0c71a86
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-03-09 18:04:07 -0500

    Updated the model to take an option hash

--- src/restful-model.js
+++ src/restful-model.js
@@ -1,36 +1,56 @@
 var RestfulModel = {
   generate: (function(){
-    return function(className){
-      console.log('Generating model ' + className);
-      var attributeNames = Array.prototype.slice.call(arguments)[1];
+    return function(options){
+      console.log('Generating model ' + options.className);
+      var attributeNames = options.fields;
 
       var Model = {
         attributesNames: attributeNames,
+        restfulURL: options.baseUrl ? options.baseUrl : '/' + options.className,
         build: function(attributes){
-          console.log('Building the ' + className);
+          console.log('Building the ' + options.className);
           return new (function(){
             function Model(attributes){
-              this.className      = className;
+              this.className      = options.className;
               this.attributeNames = attributeNames;
               this.attributes     = {};
 
-              for(var attribute in attributes){
-                var validAttribute = attributeNames.indexOf(attribute) !== -1;
-                if(validAttribute){
-                  this.attributes[attribute] = attributes[attribute];
-                  this[attribute] = attributes[attribute];
+              if(attributeNames){
+                for(var attribute in attributes){
+                  var validAttribute = attributeNames.indexOf(attribute) !== -1;
+                  if(validAttribute){
+                    this.attributes[attribute] = attributes[attribute];
+                    this[attribute] = attributes[attribute];
+                  }
                 }
+              } else {
+                this.attributes[attribute] = attributes[attribute];
+                this[attribute] = attributes[attribute];
               }
             }
             return Model;
           }())(attributes);
         },
         new: function(attributes){
-          console.log('Creating a ' + className);
+          console.log('Creating a ' + options.className);
           return this.build(attributes);
         },
         all: function(){
-          console.log('Returning all the ' + className);
+          console.log('Returning all the ' + options.className + ' records');
+          var allThis = this;
+          xhr = new XMLHttpRequest();
+          xhr.open('GET', this.restfulURL);
+          xhr.onload = function(){
+            var models = [];
+            var data   = JSON.parse(this.responseText);
+            var length = data.length;
+            console.log(length + ' ' + options.className + ' records found');
+            for(var i=0; i < length; i += 1){
+              models.push(allThis.build(data[i]));
+            }
+            return models;
+          };
+          xhr.send();
         }
       };
 
